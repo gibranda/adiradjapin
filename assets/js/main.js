6c73faf688f3661b81fcc0e215e062ba4ffaee89
boedwinangun <boedwinangun@gmail.com>
2017-12-09 19:40:22 +0700

    add testimoni section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ $( function() {
 	smoothScroll(500);
 	price();
 	priceLoad();
+	testimonial();
 
 });
 
@@ -43,5 +44,48 @@ function priceLoad() {
 
 		$('.json-load').html(spinner).load(url);
 		$('.json-title').text(title);
+	});
+}
+
+
+function testimonial() {
+	$('.testi-button').click(function() {
+		var $this = $(this),
+			position = $this.parent().children().index($this);
+
+		$('.testi-unit').removeClass('active').eq(position).addClass('active');
+		$('.testi-button').removeClass('active').eq(position).addClass('active');
+	});
+
+	$('.testi-control-next, .testi-control-prev').click(function() {
+		var $this = $(this),
+			active = $('.testi-wrap').find('.active'),
+			position = $('.testi-wrap').children().index(active),
+			num = $('.testi-unit').length;
+
+		if ($this.hasClass('testi-control-next')) 
+		{
+			if (position < num -1) 
+			{
+				$('.active').removeClass('active').next().addClass('active');
+			}
+			else 
+			{
+				$('.testi-unit').removeClass('active').first().addClass('active');
+				$('.testi-button').removeClass('active').first().addClass('active');
+			}
+		}
+		else 
+		{
+			if (position === 0) 
+			{
+				$('.testi-unit').removeClass('active').last().addClass('active');
+				$('.testi-button').removeClass('active').last().addClass('active');
+			}
+			else 
+			{
+				$('.active').removeClass('active').prev().addClass('active');
+			}
+		}
 	});
 }
